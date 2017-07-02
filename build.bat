bfe369e7d49cbbe80c929c1a73b86e106818f664
7Hazard <lolo9909@hotmail.com>
2017-07-02 03:35:19 +0200

    Updated building for CI

--- build.bat
+++ build.bat
@@ -11,9 +11,11 @@ if exist "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7
   )
 )
 
-msbuild MonoOrange\MonoOrange.vcxproj /p:Configuration=Release;Platform=x64 /p:OutputPath=..\modules\
-msbuild SharpOrange\SharpOrange.csproj /p:Configuration=Release;Platform="Any CPU" /p:OutputPath=..\modules\MonoModule\
-copy MonoOrange\mono-2.0-sgen.dll modules\MonoOrange\bin\mono-2.0-sgen.dll
-copy SharpOrange\Std-symbols.dll modules\MonoOrange\Std-symbols.dll
+msbuild MonoOrange\MonoOrange.vcxproj /p:Configuration=Release;Platform=x64 /p:OutputPath=..\bin\
+msbuild SharpOrange\SharpOrange.csproj /p:Configuration=Release;Platform="Any CPU" /p:OutputPath=..\bin\
+copy bin\MonoOrange.dll windows\MonoOrange.dll
+copy MonoOrange\mono-2.0-sgen.dll windows\MonoOrange\bin\mono-2.0-sgen.dll
+copy bin\SharpOrange.dll windows\MonoOrange\SharpOrange.dll
+copy SharpOrange\Std-symbols.dll windows\MonoOrange\Std-symbols.dll
 
 :END
--- build.sh
+++ build.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-mkdir modules
-cd modules
+cd bin
 cmake -G "Unix Makefiles" ../MonoOrange/
 make VERBOSE=1
+xbuild ../SharpOrange/SharpOrange.csproj
+cd ../
+mkdir linux
+cd linux
+cp ../bin/MonoOrange.so .
+mkdir MonoOrange
+cd MonoOrange
+cp ../bin/SharpOrange.dll .
+cp ../SharpOrange/Std-symbols.so .
