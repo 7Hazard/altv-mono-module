54ea799de5871e1d1f2286faeff90d10e7bb1948
7Hazard <lolo9909@hotmail.com>
2017-08-06 23:51:58 +0200

    xbuilds in release config

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ mkdir bin
 cd bin
 cmake -G "Unix Makefiles" ../mono-module/
 make VERBOSE=1
-xbuild ../SharpOrange/SharpOrange.csproj
+xbuild ../SharpOrange/SharpOrange.csproj /p:Configuration=Release
 cd ../
 mkdir build
 cd build
