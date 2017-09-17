691419d52e299fb2f274c9e3c9c5b19e34db2828
Hazard <lolo9909@hotmail.com>
2017-09-17 18:29:42 +0000

    Updated windows build script

--- build.bat
+++ build.bat
@@ -12,9 +12,11 @@ if exist "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7
 )
 
 msbuild mono-module\mono-module.vcxproj /p:Configuration=Release;Platform=x64 /p:OutputPath=..\bin\
+msbuild SharpOrange\SharpOrange.csproj /p:Configuration=Debug;Platform="Any CPU";AllowUnsafeBlocks=true;SkipBuildPackage=false;RestorePackages=false /p:PreBuildEvent= /p:PostBuildEvent= /p:OutputPath=..\bin\
 msbuild SharpOrange\SharpOrange.csproj /p:Configuration=Release;Platform="Any CPU";AllowUnsafeBlocks=true;SkipBuildPackage=false;RestorePackages=false /p:PreBuildEvent= /p:PostBuildEvent= /p:OutputPath=..\bin\
 mkdir build
 copy mono-module\bin\mono-module.dll build\mono-module.dll
 copy mono-module\mono-2.0-sgen.dll build\mono-module\bin\mono-2.0-sgen.dll
 copy bin\SharpOrange.dll build\mono-module\SharpOrange.dll
+copy bin\SM\SharpOrangeSM.dll build\mono-module\SharpOrangeSM.dll
 :END
