00a218c6711e326ef6caedc2b1239f61e40aa873
Hazard <lolo9909@hotmail.com>
2017-09-19 02:13:31 +0200

    Fixed linux build

--- Server/linux/resources/SharpTest/SharpTest.cs
+++ Server/linux/resources/SharpTest/SharpTest.cs
@@ -16,9 +16,8 @@ namespace SharpTest
 		void Event_OnPlayerConnect (SharpOrange.Objects.Player player)
 		{
 			Server.Print ("PLAYER CONNECTED");
-			//Vector3 pos = player.Position;
-			//player.Position = new Vector3(pos.x, pos.y, pos.z+1000);
-			player.Position = new Vector3(100, 100, 100);
+			Vector3 pos = player.Position;
+			player.Position = new Vector3(pos.x, pos.y, pos.z+1000);
 		}
 	}
 }
